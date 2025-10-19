db2067d8977578fe2668e7ff53ac541e1fb9a0b1
Meghana Thota <thota.meg@northeastern.edu>
2025-10-19 18:44:06 -0400

    created universitypersona

---
+++ group_lab/src/Universitypersona/UserAccount.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Universitypersona;
+
+/**
+ *
+ * @author kal bugrara
+ */
+
+import Universitypersona.Person;
+import info5100.university.example.workareas.Workarea;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class UserAccount {
+    Person person;
+    Workarea landingworkarea;
+    
+    public UserAccount(Person p){
+        person = p;
+    }
+    
+}
