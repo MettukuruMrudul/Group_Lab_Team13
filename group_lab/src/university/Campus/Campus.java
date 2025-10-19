367f93e620dd153828e587dd74cab8cc761ba485
krishi <parekh.krishi@northeastern.edu>
2025-10-19 02:45:34 -0400

    created address.java, Building.java, campus.java and campusmain.java

---
+++ group_lab/src/university/Campus/Campus.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package university.Campus;
+
+import university.Campus.Building;
+import university.Campus.Address;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kal bugrara
+ */
+public class Campus {
+    
+    Address address;
+    ArrayList<Building> buildings;
+    public Campus(Address a){
+    
+        address = a;
+        buildings = new ArrayList();
+        
+    }
+    
+}
